544bc74348f8b1b8e33f8df6f247cd818e3b2819
Danylo Paliienko <it18@eurocommerce.biz>
2025-01-08 11:02:36 +0200

    removed mongoose

--- src/schemas/web3-bb-program.schema.ts
+++ src/schemas/web3-bb-program.schema.ts
@@ -1,41 +1,13 @@
-// web3-bb-program.schema.ts
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
-
-@Schema()
-export class Web3BBProgram extends Document {
-  @Prop({ required: true })
+export interface Web3BBProgram {
   program: string;
-
-  @Prop({ required: true })
   slug: string;
-
-  @Prop({ required: true })
   platform: string;
-
-  @Prop({ required: true })
   imageUrl: string;
-
-  @Prop({ required: true })
   originalUrl: string;
-
-  @Prop({ type: [String], default: [] })
   languages: string[];
-
-  @Prop({ required: true })
   maxReward: number;
-
-  @Prop({ required: true })
   rewardsPool: number;
-
-  @Prop({ required: true })
   rewardsToken: string;
-
-  @Prop({ required: true })
   startDate: Date;
-
-  @Prop({ type: [String], default: [] })
   tags: string[];
 }
-
-export const Web3BBProgramSchema = SchemaFactory.createForClass(Web3BBProgram);
